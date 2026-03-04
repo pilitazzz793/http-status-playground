d8b854fa637a92fc9f8569a89d913f57c2b71167
Pilar <nadaalpilarr@gmail.com>
2026-03-03 21:38:06 -0300

    nuestro app.js solo configura

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,38 +1,16 @@
-//aqui se encuentra nuestro servidor, el cual se encarga de recibir las peticiones y enviar las respuestas
+//aqui se encuentra nuestro servidor
 
 const express = require("express");
 require("dotenv").config();
+
+const statusRoutes = require('/routes/status.routes');
 const pool = require("./db");
 
 const app = express();
 app.use(express.json());
+app.use('/status', statusRoutes);
 
-//ruta de prueba
-app.get("/", async (req, res) =>{
-    try {
-        const result = await pool.query("SELECT NOW()");
-        res.status(200).json({
-            message: "servidor y db funcionando correctamente",
-            time: result.rows[0],
-        });
-    } catch(error) {
-        res.status(500).json({
-            error :" error al conectar con la base de datos",
-        });
-        
-    }
-});
 
-//creamos nueva ruta para traer los estados
-app.get("/status", async (req, res) =>{
-    try{
-        const result = await pool.query("SELECT * FROM status_codes");
-        res.json(result.rows);
-    }catch(error){
-        console.error(error);
-        res.status(500).json({ error: "error al obtener los status"})
-    }
-});
 
 const PORT = process.env.PORT || 3000;
 
