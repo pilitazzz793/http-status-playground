81b799716081dd195f760de5b78b186ddbc93bab
Pilar <nadaalpilarr@gmail.com>
2026-03-05 11:23:38 -0300

    se arreglo un error de sintaxis que impedia poder levantar el servidor

--- Backend/src/routes/status.routes.js
+++ Backend/src/routes/status.routes.js
@@ -5,7 +5,7 @@ const statusController = require('../controllers/status.controller');
 //ruta para obtener todos los status codes
 
 router.get('/', statusController.getAllStatus);
-router.get('/', get.statusController.getStatusById);
+router.get('/', statusController.getStatusById);
 router.post('/', statusController.createStatus);
 router.delete('/', statusController.deleteStatus);
 router.put('/:id', statusController.updateStatus);
