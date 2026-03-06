a387c1480e2ca8c2b38c5c843c63859d0d4e539c
Pilar <nadaalpilarr@gmail.com>
2026-03-06 20:06:33 -0300

    componente principal listo

--- Fronted/src/App.jsx
+++ Fronted/src/App.jsx
@@ -1,29 +1,14 @@
-import { useEffect, useState } from "react";
+import Home from "./pages/Home";
 
 function App(){
-  const[statusList, setStatusList]= useState([]);
+  return(
+    <div>
+      <Home/>
+    </div>
+  );
 
-  useEffect(()=>{
-    fetch('http://localhost:3000/status')
-    .then((res) => res.json())
-    .then((data) => setStatusList(data))
-    .catch((error)=> console.error(error));
-  }, []);
-return(
-  <div>
-    <h1>HTTP status playground</h1>
-
-    <h2>Lista de status</h2>
-
-    {statusList.map((status) =>(
-      <div key={status.id}>
-        <h3>{status.code} - {status.message}</h3>
-        <p>{status.description}</p>
-        </div>
-    ))}
-  </div>
-)
 }
+
 
 
 export default App;
