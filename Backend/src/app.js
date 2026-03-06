7af5a7098d37ba76e1f391d2dbcea329a531755c
Pilar <nadaalpilarr@gmail.com>
2026-03-06 11:51:20 -0300

    instalacion de cors realizada

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,12 +1,14 @@
 //aqui se encuentra nuestro servidor
 
 const express = require("express");
+const cors = require('cors');
 require("dotenv").config();
 
 const statusRoutes = require('./routes/status.routes');
 const pool = require("./db");
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use('/status', statusRoutes);
 
--- Fronted/src/App.jsx
+++ Fronted/src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+function App(){
+  const[statusList, setStatusList]= useState([]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  useEffect(()=>{
+    fetch('http://localhost:3000/status')
+    .then((res) => res.json())
+    .then((data) => setStatusList(data))
+    .catch((error)=> console.error(error));
+  }, []);
+return(
+  <div>
+    <h1>HTTP status playground</h1>
+
+    <h2>Lista de status</h2>
+
+    {statusList.map((status) =>(
+      <div key={status.id}>
+        <h3>{status.code} - {status.message}</h3>
+        <p>{status.description}</p>
+        </div>
+    ))}
+  </div>
+)
 }
 
-export default App
+
+export default App;
