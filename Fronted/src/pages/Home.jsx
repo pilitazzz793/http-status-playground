288f6ddcdb49ed653e63d2dfdf0592aed18bcf93
Pilar <nadaalpilarr@gmail.com>
2026-03-06 19:57:32 -0300

    componente home donde organizamos los componentes

---
+++ Fronted/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import StatusList from "../components/StatusList";
+
+function Home(){
+    return(
+        <div>
+            <h1>HTTP Status Playground</h1>
+
+            <StatusList/>
+        </div>
+    );
+}
+
+export default Home;
