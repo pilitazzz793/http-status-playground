15fecd90d7e823e3552ebdc9aa98e7b413fad025
Pilar <nadaalpilarr@gmail.com>
2026-03-11 21:27:49 -0300

    probando nuevos estados

--- Fronted/src/components/StatusList.jsx
+++ Fronted/src/components/StatusList.jsx
@@ -1,28 +1,32 @@
 import { useEffect, useState } from "react";
 import statusCard from "./StatusCard";
-import {getStatus} from "../services/StatusService";
+import { getStatus } from "../services/StatusService";
 
-function StatusList(){
+function StatusList() {
 
-    const [statusList, setStatusList] = useState([]);
+    const [statusList, setStatusList] = useState([
+         {id:1, code:200, message:"OK", description:"Todo bien"}
+    ]);
 
-    useEffect(() =>{
+    useEffect(() => {
         loadStatus();
     }, []);
 
-    const loadStatus = async ()=>{
-        try{
+    const loadStatus = async () => {
+        try {
             const data = await getStatus();
+            console.log(data);
             setStatusList(data);
-        }catch(error){
+        } catch (error) {
             console.error('error al cargar el status', error);
         }
     };
 
-    return(
+    return (
         <div>
-            {statusList.map((status)=>{
-                <statusCard key ={status.id} status={status}/>
+            <h1>Componente cargado</h1>
+            {statusList.map((status) => {
+                <statusCard key={status.id} status={status} />
             })}
         </div>
     );
